a2a6895b800c5e2d965c4ca2703e6a1be07b0d43
Sebastian Jäger <git@sebastian-jaeger.me>
2023-12-09 18:32:14 +0100

    simple CLI for basic preperation

---
+++ README.md
@@ -0,0 +1,35 @@
+# Prepare [Mintos](https://www.mintos.com/) Statements for [Portfolio Performance](https://www.portfolio-performance.info/) Import
+
+Simple CLI that 
+1. reads all CSV files in the current working directory
+2. concatenates them
+3. processes the statements
+   1. map Mintos types to Portfolio Performance types
+   2. sums all statements for each day and type to minimize rounding inaccuracies ([see here](https://github.com/portfolio-performance/portfolio/issues/1682#issuecomment-667650637))
+4. export proccessed data as CSV to import into Portfolio Performance
+
+## Some Notes
+Simple tool implemented to make my personal workflow more convenient. However, maybe it also helps you.
+
+**Tested for:**
+- German Mintos exports and Portfolio Performance
+- MacOS
+- Python 10
+- Poetry installed package
+
+I don't see a reason why it shouldn't work for another technical setup. Only `pandas` is a requirement. PR's are welcome to make the code more generic regarding language settings.
+
+## Usage
+1. clone repo: `git clone git@github.com:se-jaeger/mintos-statement-prep-for-portfolio-performance.git`
+2. install package: `poetry install`
+3. download statements form Mintos and add to `data` directory
+4. run the tool and follow instructions
+    ```python
+    ➜ $ prep-mintos-statements
+    Using the following files: ['2023-12-09.csv', '2022-account-statement.csv', '2021-36326852-eur-de-statement.csv', '2020-36326852-eur-de-statement.csv', '2023-account-statement.csv']
+    OK (Y/N)? y
+
+    Using the following output file: 2023-12-09.csv
+    OK (Y/N)? y
+    ```
+5. import the output file into Portfolio Performance (it should detect duplicates and not reimport them)
---
+++ mintos_statement_prep_for_portfolio_performance/main.py
@@ -0,0 +1,73 @@
+import sys
+from datetime import datetime
+from pathlib import Path
+
+import pandas as pd
+
+columns_of_interest = ["Datum", "Typ", "Wert", "Buchungswährung"]
+how_many_types_expected = 5
+
+type_mapping = {
+    # Map Mintos types to Portfolio Performance types
+    "Tilgungszahlungen": None,
+    "Zinszahlungen": "Zinsen",
+    "Erhaltene Zinsen aus Kreditrückkauf": "Zinsen",
+    "Erhaltene Tilgung aus Kreditrückkauf": None,
+    "Übertragungsabgleich der Verzugszinseinnahmen": "Zinsen",
+    "Investitionen in Darlehen": None,
+    "Zinseinnahmen aus ausstehenden Zahlungen": "Zinsen",
+    "Steuereinbehalt": "Steuern",
+    "Erhaltene Tilgung aus Rückkauf kleiner Kreditteile": None,
+    "Zweitmarkt-Transaktion – Ab- oder Aufschlag": "Zinsen",  # noqa: RUF001
+    "Zweitmarkt-Transaktion": None,
+    "Zweitmarktgebühr": "Gebühren",
+    "Einzahlungen": "Einlage",
+    "Abhebung": "Entnahme",
+}
+
+
+def _ask_if_ok(msg: str) -> None:
+    print(msg)  # noqa: T201
+    asdf = input("OK (Y/N)? ")
+    print()  # noqa: T201
+
+    if asdf not in ["Y", "y"]:
+        sys.exit(1)
+
+
+def start() -> None:
+    """Entrypoint for CLI.
+
+    Raises:
+        Exception: _description_
+    """
+    statement_files = list(Path().glob("*.csv"))
+
+    _ask_if_ok(f"Using the following files: {[x.name for x in statement_files]}")
+
+    data = (
+        pd.concat([pd.read_csv(x) for x in statement_files])
+        .convert_dtypes()
+        .assign(
+            Datum=lambda df: pd.to_datetime(df["Datum"]).dt.date,
+            Typ=lambda df: df["Zahlungsart"].replace(type_mapping),
+            Wert=lambda df: df.groupby([df["Datum"], "Typ"])["Umsatz"].transform("sum").round(5),
+        )
+        .rename(columns={"Währung": "Buchungswährung"})
+        .dropna(subset="Typ")
+        .drop_duplicates(columns_of_interest)
+    )
+
+    if len(data["Typ"].unique().tolist()) != how_many_types_expected:
+        msg = f"Expecting {how_many_types_expected} distinct values here. Please check if Mintos export changed."
+        raise Exception(msg)  # noqa: TRY002
+
+    output_file_name = f"{datetime.now().strftime('%Y-%m-%d')}.csv"  # noqa: DTZ005
+    _ask_if_ok(f"Using the following output file: {output_file_name}")
+
+    data[columns_of_interest].to_csv(
+        output_file_name,
+        index=False,
+        sep=";",
+        decimal=",",
+    )
